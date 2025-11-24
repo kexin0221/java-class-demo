f845aecf666685f379596a46435f07f12ad50d10
mou-kexin <2584857586@qq.com>
2025-11-24 20:04:56 +0800

    Java-class-demo

--- src/chapter31/MultiThreadServer.java
+++ src/chapter31/MultiThreadServer.java
@@ -11,104 +11,100 @@ import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class MultiThreadServer extends Application {
-  // Text area for displaying contents
-  private TextArea ta = new TextArea();
-  
-  // Number a client
-  private int clientNo = 0;
+    // Text area for displaying contents
+    private final TextArea ta = new TextArea();
 
-  @Override // Override the start method in the Application class
-  public void start(Stage primaryStage) {
-    // Create a scene and place it in the stage
-    Scene scene = new Scene(new ScrollPane(ta), 450, 200);
-    primaryStage.setTitle("MultiThreadServer"); // Set the stage title
-    primaryStage.setScene(scene); // Place the scene in the stage
-    primaryStage.show(); // Display the stage
+    // Number a client
+    private int clientNo = 0;
 
-    new Thread( () -> {
-      try {
-        // Create a server socket
-        ServerSocket serverSocket = new ServerSocket(8000);
-        ta.appendText("MultiThreadServer started at " 
-          + new Date() + '\n');
-    
-        while (true) {
-          // Listen for a new connection request
-          Socket socket = serverSocket.accept();
-    
-          // Increment clientNo
-          clientNo++;
-          
-          Platform.runLater( () -> {
-            // Display the client number
-            ta.appendText("Starting thread for client " + clientNo +
-              " at " + new Date() + '\n');
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(new ScrollPane(ta), 450, 200);
+        primaryStage.setTitle("MultiThreadServer"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
 
-            // Find the client's host name, and IP address
-            InetAddress inetAddress = socket.getInetAddress();
-            ta.appendText("Client " + clientNo + "'s host name is "
-              + inetAddress.getHostName() + "\n");
-            ta.appendText("Client " + clientNo + "'s IP Address is "
-              + inetAddress.getHostAddress() + "\n");
-          });
-          
-          // Create and start a new thread for the connection
-          new Thread(new HandleAClient(socket)).start();
-        }
-      }
-      catch(IOException ex) {
-        System.err.println(ex);
-      }
-    }).start();
-  }
-  
-  // Define the thread class for handling new connection
-  class HandleAClient implements Runnable {
-    private Socket socket; // A connected socket
+        new Thread( () -> {
+            try {
+                // Create a server socket
+                ServerSocket serverSocket = new ServerSocket(8000);
+                ta.appendText("MultiThreadServer started at " + new Date() + '\n');
 
-    /** Construct a thread */
-    public HandleAClient(Socket socket) {
-      this.socket = socket;
+                while (true) {
+                    // Listen for a new connection request
+                    Socket socket = serverSocket.accept();
+
+                    // Increment clientNo
+                    clientNo++;
+
+                    Platform.runLater( () -> {
+                        // Display the client number
+                        ta.appendText("Starting thread for client " + clientNo + " at " + new Date() + '\n');
+
+                        // Find the client's host name, and IP address
+                        InetAddress inetAddress = socket.getInetAddress();
+                        ta.appendText("Client " + clientNo + "'s host name is " + inetAddress.getHostName() + "\n");
+                        ta.appendText("Client " + clientNo + "'s IP Address is " + inetAddress.getHostAddress() + "\n");
+                    });
+
+                    // Create and start a new thread for the connection
+                    new Thread(new HandleAClient(socket)).start();
+                }
+            }
+            catch(IOException ex) {
+                System.err.println(ex);
+            }
+        }).start();
     }
 
-    /** Run a thread */
-    public void run() {
-      try {
-        // Create data input and output streams
-        DataInputStream inputFromClient = new DataInputStream(
-          socket.getInputStream());
-        DataOutputStream outputToClient = new DataOutputStream(
-          socket.getOutputStream());
+    // Define the thread class for handling new connection
+    class HandleAClient implements Runnable {
+        private final Socket socket; // A connected socket
 
-        // Continuously serve the client
-        while (true) {
-          // Receive radius from the client
-          double radius = inputFromClient.readDouble();
-
-          // Compute area
-          double area = radius * radius * Math.PI;
-
-          // Send area back to the client
-          outputToClient.writeDouble(area);
-          
-          Platform.runLater(() -> {
-            ta.appendText("radius received from client: " +
-              radius + '\n');
-            ta.appendText("Area found: " + area + '\n');
-          });
+        /** Construct a thread */
+        public HandleAClient(Socket socket) {
+            this.socket = socket;
         }
-      }
-      catch(IOException ex) {
-        ex.printStackTrace();
-      }
+
+        /** Run a thread */
+        public void run() {
+            try {
+                // Create data input and output streams
+                DataInputStream inputFromClient = new DataInputStream(
+                        socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(
+                        socket.getOutputStream());
+
+                // Continuously serve the client
+                while (true) {
+                    // Receive radius from the client
+                    double radius = inputFromClient.readDouble();
+
+                    // Compute area
+                    double area = radius * radius * Math.PI;
+
+                    // Send area back to the client
+                    outputToClient.writeDouble(area);
+
+                    Platform.runLater(() -> {
+                        ta.appendText("radius received from client: " +
+                                radius + '\n');
+                        ta.appendText("Area found: " + area + '\n');
+                    });
+                }
+            }
+            catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
-  }
-  
-  /**
-   * The main method is only needed for the IDE with limited
-   * JavaFX support. Not needed for running from the command line.
-   */
-  public static void main(String[] args) {
-    launch(args);
-  }
+
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
